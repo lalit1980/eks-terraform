b9c443b26f6fd921567a8d923526158ea8607ceb
Lalit Yadav <lalit1980@gmail.com>
2022-08-30 13:16:47 +0800

    Working terra code

--- provider.tf
+++ provider.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    kind = {
-      source  = "kyma-incubator/kind"
-      version = "0.0.9"
-    }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -26,18 +22,18 @@ terraform {
   }
 }
 provider "aws" {
-  region  = "ap-southeast-1"
+  region                  = "ap-southeast-1"
   shared_credentials_file = "/Users/lalit/.aws/credentials"
-  profile = "terraform"
+  profile                 = "terraform"
 }
 terraform {
   backend "s3" {
-    bucket         = "eks-terraform-state-daddyzi"
-    key            = "daddyzi/dev/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "eks-terraform-state-daddyzi"
+    bucket                  = "eks-terraform-state-daddyzi"
+    key                     = "daddyzi/dev/terraform.tfstate"
+    region                  = "ap-southeast-1"
+    dynamodb_table          = "eks-terraform-state-daddyzi"
     shared_credentials_file = "/Users/lalit/.aws/credentials"
-    profile = "terraform"
+    profile                 = "terraform"
   }
 }
 
